9af698a705cbc9548f986fe4da6bfd2c82a875b5
hymanbuer <505228589@qq.com>
2018-07-31 00:38:13 +0800

    refined

--- assets/scripts/base/engine/CCLoaderHelper.js
+++ assets/scripts/base/engine/CCLoaderHelper.js
@@ -1,22 +1,9 @@
 
 const CCLoaderHelper = module.exports = {};
 
-CCLoaderHelper.queryAsset = function (uuid) {
-    return new Promise((resolve, reject) => {
-        cc.AssetLibrary.queryAssetInfo(uuid, (err, url, raw) => {
-            const asset = cc.loader.getRes(url);
-            resolve(asset);
-        });
-    });
-}
-
 // if the res was already loaded then return directly,
 // other then load by cc.loader.loadRes.
 CCLoaderHelper.loadResByUrl = function (url, type) {
-    const res = cc.loader.getRes(url, type);
-    if (res)
-        return new Promise((resolve, reject) => resolve(res));
-    
     return new Promise((resolve, reject) => {
         cc.loader.loadRes(url, type, (err, asset) => {
             if (err)
