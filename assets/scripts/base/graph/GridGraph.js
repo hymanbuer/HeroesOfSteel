bc7f7f36d98dbb4a0813b8c233ce4dc1ec27a238
hymanbuer <505228589@qq.com>
2018-08-02 23:40:40 +0800

    refined

--- assets/scripts/base/graph/GridGraph.js
+++ assets/scripts/base/graph/GridGraph.js
@@ -29,43 +29,7 @@ class GridNode {
     get y () { return this._y; }
 }
 
-function indexes2grids(graph, index1, index2) {
-    const grid1 = graph.index2grid(index1);
-    const grid2 = graph.index2grid(index2);
-    return [grid1.x, grid1.y, grid2.x, grid2.y];
-}
-
 class GridGraph {
-    static createHeuristic (graph, type) {
-        switch (type) {
-        case GridGraph.HeuristicType.Manhattan:
-            return (index1, index2) => {
-                const [x1, y1, x2, y2] = indexes2grids(graph, index1, index2);
-                const d1 = Math.abs(x1 - x2);
-                const d2 = Math.abs(y1 - y2);
-                return d1 + d2;
-            };
-
-        case GridGraph.HeuristicType.Diagonal:
-            return (index1, index2) => {
-                const [x1, y1, x2, y2] = indexes2grids(graph, index1, index2);
-                const D = 1;
-                const D2 = Math.SQRT2;
-                const d1 = Math.abs(x1 - x2);
-                const d2 = Math.abs(y1 - y2);
-                return (D * (d1 + d2)) + ((D2 - (2 * D)) * Math.min(d1, d2));
-            };
-
-        case GridGraph.HeuristicType.MaxDiagonal:
-            return (index1, index2) => {
-                const [x1, y1, x2, y2] = indexes2grids(graph, index1, index2);
-                const d1 = Math.abs(x1 - x2);
-                const d2 = Math.abs(y1 - y2);
-                return Math.max(d1, d2);
-            };
-        }
-    }
-
     constructor (maxRows, maxColumns, grids, isDiagonal) {
         this._maxRows = maxRows;
         this._maxColumns = maxColumns;
@@ -173,11 +137,5 @@ class GridGraph {
             this._nodes[neighbor].neighbors = null;
     }
 }
-
-GridGraph.HeuristicType = cc.Enum({
-    Manhattan: -1,
-    Diagonal: -1,
-    MaxDiagonal: -1,
-})
 
 module.exports = GridGraph;
